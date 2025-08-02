3060609adb10ffd9084305a269e5ec4a033592a9
comunidadudp <comunidad.udp.shared@gmail.com>
2025-08-02 01:11:59 -0500

    Add temporal Api for business services test

---
+++ EntryLog.Api/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntryLog.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TestController(
+        IAppUserServices appUserServices) : ControllerBase
+    {
+        [HttpPost("register-employee")]
+        public async Task<object> CreateUserEmployeeAsync([FromBody] CreateEmployeeUserDTO employeeUserDTO)
+        {
+            return await appUserServices.RegisterEmployeeAsync(employeeUserDTO);
+        }
+    }
+}
