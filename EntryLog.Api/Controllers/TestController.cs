c19e31a32ae74e027e718f560b113f370ed7bebd
comunidadudp <comunidad.udp.shared@gmail.com>
2025-08-03 01:00:07 -0500

    Add ImageBBOptions to json config and endpoint for testing

--- EntryLog.Api/Controllers/TestController.cs
+++ EntryLog.Api/Controllers/TestController.cs
@@ -1,18 +1,79 @@
 ﻿using EntryLog.Business.DTOs;
 using EntryLog.Business.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace EntryLog.Api.Controllers
 {
     [Route("api")]
     [ApiController]
     public class TestController(
-        IAppUserServices appUserServices) : ControllerBase
+        IAppUserServices appUserServices,
+        IWorkSessionServices workSessionServices) : ControllerBase
     {
-        [HttpPost("register-employee")]
+        [HttpPost("user/register-employee-user")]
         public async Task<object> CreateUserEmployeeAsync([FromBody] CreateEmployeeUserDTO employeeUserDTO)
         {
-            return await appUserServices.RegisterEmployeeAsync(employeeUserDTO);
+            (bool success, string message, LoginResponseDTO? data) = await appUserServices.RegisterEmployeeAsync(employeeUserDTO);
+            return Ok(new
+            {
+                success,
+                message,
+                data
+            });
         }
+
+
+        [HttpPost("user/login")]
+        public async Task<object> UserLoginAsync([FromBody] UserCredentialsDTO credentialsDTO)
+        {
+            (bool success,string message, LoginResponseDTO? data) = await appUserServices.UserLoginAsync(credentialsDTO);
+            return Ok(new
+            {
+                success,
+                message,
+                data
+            });
+        }
+
+        [HttpPost("user/recovery-start")]
+        public async Task<object> AccountRecoveryStartAsync([FromBody] string username)
+        {
+            (bool success, string message) = await appUserServices.AccountRecoveryStartAsync(username);
+            return Ok(new
+            {
+                success,
+                message,
+            });
+        }
+
+        [HttpPost("user/recovery-complete")]
+        public async Task<object> AccountRecoveryCompleteAsync([FromBody] AccountRecoveryDTO recoveryDTO)
+        {
+            (bool success, string message) = await appUserServices.AccountRecoveryCompleteAsync(recoveryDTO);
+            return Ok(new
+            {
+                success,
+                message,
+            });
+        }
+
+        [HttpPost("test-image")]
+        [Consumes("multipart/form-data")]
+        public async Task<object> ImageTestAsync([FromForm] ImageUploadRequest request)
+        {
+            (bool success, string message) = await workSessionServices.ImageTestAsync(request.Image);
+            return Ok(new
+            {
+                success,
+                message,
+            });
+        }
+    }
+
+    public class ImageUploadRequest
+    {
+        [Required]
+        public IFormFile Image { get; set; }
     }
 }
--- EntryLog.Api/Program.cs
+++ EntryLog.Api/Program.cs
@@ -15,7 +15,6 @@ namespace EntryLog.Api
                 .AddDataServices(builder.Configuration)
                 .AddBusinessServices(builder.Configuration);
 
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
