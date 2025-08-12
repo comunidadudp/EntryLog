942dc73f3453c5f0ab2df9a08e07988a9ea67d09
comunidadudp <comunidad.udp.shared@gmail.com>
2025-08-12 11:39:48 -0500

    Add Web Application

---
+++ EntryLog.Web/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using EntryLog.Business.DTOs;
+using EntryLog.Business.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntryLog.Web.Controllers
+{
+    public class AccountController(IAppUserServices userServices) : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult RegisterEmployeeUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<JsonResult> RegisterEmployeeUserAsync(CreateEmployeeUserDTO model)
+        {
+            (bool success, string message, LoginResponseDTO? data) = await userServices.RegisterEmployeeAsync(model);
+
+            if (success)
+            {
+                //loguear al empleado
+                
+
+                return Json(new
+                {
+                    success,
+                    path = "/main"
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    success,
+                    message
+                });
+            }
+        }
+    }
+}
---
+++ EntryLog.Web/Program.cs
@@ -0,0 +1,34 @@
+using EntryLog.Business;
+using EntryLog.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddDataServices(builder.Configuration)
+    .AddBusinessServices(builder.Configuration);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    //app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=RegisterEmployeeUser}/{id?}");
+
+app.Run();
