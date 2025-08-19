bb0cd5f268adb7a91db3c58ec9424c14a5971ea1
comunidadudp <comunidad.udp.shared@gmail.com>
2025-08-19 17:04:42 -0500

    Add TimeFunctions in Utils

---
+++ EntryLog.Business/Utils/TimeFunctions.cs
@@ -0,0 +1,13 @@
+﻿namespace EntryLog.Business.Utils
+{
+    public static class TimeFunctions
+    {
+        private const string SA_PACIFIC_STANDARD_TIME = "SA Pacific Standard Time";
+
+        public static DateTime GetSAPacificStandardTime()
+            => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById(SA_PACIFIC_STANDARD_TIME));
+
+        public static DateTime GetSAPacificStandardTime(DateTime utcDate)
+            => TimeZoneInfo.ConvertTimeFromUtc(utcDate, TimeZoneInfo.FindSystemTimeZoneById(SA_PACIFIC_STANDARD_TIME));
+    }
+}
